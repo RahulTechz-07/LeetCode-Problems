dcf55177e7418514313363bda14cb91f355797b9
Rahul <132188597+RahulTechz-07@users.noreply.github.com>
2024-06-24 11:52:24 +0530

    Time: 16 ms (27.73%) | Memory: 11.6 MB (60.55%) - LeetSync

---
+++ 1920-determine-color-of-a-chessboard-square/determine-color-of-a-chessboard-square.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def squareIsWhite(self, coordinates):
+        alfa = "abcdefghijklmnopqrstuvwxyz"
+        if alfa.index(coordinates[0]) % 2 == 0:
+            if int(coordinates[1]) % 2 == 0:
+                return True
+            else:
+                return False
+        else:
+            if int(coordinates[1]) % 2 == 0:
+                return False
+            else:
+                return True    
+        
