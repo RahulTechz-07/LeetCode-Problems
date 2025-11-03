86b44ba71c1fa9e4c4d9367ab5a932d6053743bd
Rahul <132188597+RahulTechz-07@users.noreply.github.com>
2025-11-03 12:39:36 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (5.50%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -3,11 +3,9 @@ class Solution {
         int s=0;
         for(int i=0;i<mat.length;i++)
         s+=mat[i][i];
-        System.out.println(s);
         for(int i=mat.length-1,j=0;i>=0  ;i--,j++)
         if( i!=j)
         s+=mat[i][j];
-        System.out.println(s);
         return s;
     }
 }
